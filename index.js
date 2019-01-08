e600d0163665099f524eaea98d6b85167f5b9ff7
Francesco Piantini <francesco.piantini@gmail.com>
2019-01-08 07:51:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const jsonData = fs.readFileSync(`${__dirname}/data/treks.json`, 'utf-8');
+
+const trekData = JSON.parse(jsonData);
+
+console.log(trekData);
+
+const server = http.createServer((req, res) => {
+
+  const pathName = url.parse(req.url, true).pathname;
+
+  console.log(pathName);
+  
+  // PRODUCT OVERVIEW -------------------------------------------------
+  if (pathName === '/') {
+      res.writeHead(200, {'Content-type': 'text/html'});
+      res.end('prova');
+  }
+  else if (pathName === '/favicon.ico') {
+    // Da capire se funziona
+    fs.readFile(`${__dirname}/data/img/favicon.png`, (err,data) => {
+      res.writeHead(200, {'Content-type': 'image/png'});
+      res.end(data);
+    });
+  }
+  
+  // NOT FOUND -------------------------------------------------------
+  else {
+      res.writeHead(404, {'Content-type': 'text/html'});
+      res.end('ERROR: Page not found in the server');
+  }
+});
+
+server.listen(1337, 'localhost', () => {
+  console.log('Server started');
+});
+
