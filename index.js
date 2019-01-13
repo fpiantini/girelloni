4c48436a212c3dc75046d87fc0b7cebe28b2ef41
Francesco Piantini <francesco.piantini@gmail.com>
2019-01-13 16:28:34 +0100

    completed landing page with fake nations list

--- index.js
+++ index.js
@@ -15,16 +15,32 @@ const server = http.createServer((req, res) => {
 
   console.log(pathName);
 
-  // PRODUCT OVERVIEW -------------------------------------------------
+  // MAIN PAGE -------------------------------------------------------
   if (pathName === '/') {
-      res.writeHead(200, {'Content-type': 'text/html'});
-      res.end('prova');
+    res.writeHead(200, {'Content-type': 'text/html'});
+
+    const nationsList = extractListOfNationsFromTrek(trekData);
+    fs.readFile(`${__dirname}/pages/html_templates/template-mainpage.html`, 'utf-8', (err, data) => {
+      let mainpageOutput = data;
+      fs.readFile(`${__dirname}/pages/html_templates/template-nation.html`, 'utf-8', (err, data) => {
+        nationsOutput = nationsList.sort().map((nation, ndx) => parseNationsTemplate(data, nation, ndx)).join('');
+        mainpageOutput = mainpageOutput.replace(/{%NATIONS%}/g, nationsOutput);
+        res.end(mainpageOutput);
+      });
+    });
   }
-  else if (pathName === '/favicon.ico') {
-    // Da capire se funziona
-    fs.readFile(`${__dirname}/data/img/favicon.png`, (err,data) => {
-      res.writeHead(200, {'Content-type': 'image/png'});
-      res.end(data);
+  // IMAGES ----------------------------------------------------------
+  else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathName)) {
+    fs.readFile(`${__dirname}/pages/imgs${pathName}`, (err,data) => {
+        res.writeHead(200, {'Content-type': 'image/jpg'});
+        res.end(data);
+    });
+  }
+  // STYLE SHEET -----------------------------------------------------
+  else if ((/\.css$/i).test(pathName)) {
+    fs.readFile(`${__dirname}/pages/css${pathName}`, (err,data) => {
+        res.writeHead(200, {'Content-type': 'text/css'});
+        res.end(data);
     });
   }
 
@@ -38,4 +54,19 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
+// ------------------------------------------------------------------------------------
+function extractListOfNationsFromTrek(trekData) {
+  // FIXME --- TO BE COMPLETED --- extract list of nations from trekData ---
+  return ['Italia', 'Islanda'];
+}
+
+// ------------------------------------------------------------------------------------
+function parseNationsTemplate(data, nation, ndx) {
+  const cssClass =  ndx % 2 ? 'divdark' : 'divlight';
+  data = data.replace(/{%NATION%}/g, nation);
+  data = data.replace(/{%NATION_LOWERCASE%}/g, nation.toLowerCase());
+  data = data.replace(/{%CSSCLASS%}/g, cssClass);
+  return data;
+}
 
